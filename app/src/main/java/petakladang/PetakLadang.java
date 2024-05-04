846fa4426b41440bbddff87d7d5fa6757f84ff74
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-04 21:10:12 +0700

    feat: petakladang

--- app/src/main/java/petakladang/PetakLadang.java
+++ app/src/main/java/petakladang/PetakLadang.java
@@ -2,9 +2,15 @@ package petakladang;
 import java.util.ArrayList;
 
 class PetakLadang<T extends Card> {
-  private List<T> petakLadang;
+  private List<List<T>> petakLadang;
+  private static final int MAX_ROW = 4;
+  private static final int MAX_COL = 5;
 
   public PetakLadang() {
-    petakLadang = new ArrayList<T>();
+    petakLadang = new ArrayList<T>(MAX_ROW);
+
+    for (int i = 0; i < MAX_ROW; i++) {
+      petakLadang.add(new ArrayList<T>(MAX_COL));
+    }
   }
 }
