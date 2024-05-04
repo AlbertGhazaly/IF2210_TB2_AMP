a582dd2088997833ea88b4988660209f99a19df7
Albert Ghazaly <albert260306@gmail.com>
2024-05-04 21:09:36 +0700

    feat: update Hewan class

--- app/src/main/java/card/Hewan.java
+++ app/src/main/java/card/Hewan.java
@@ -5,6 +5,28 @@ public class Hewan extends Card{
     private int berat;
     private int beratPanen;
 
+    public Hewan(){
+        super();
+        this.kategori = "";
+        this.berat = 0;
+        this.beratPanen = 0;
+    }
+
+    public Hewan(String nama, String imgPath,String kategori, int berat, int beratPanen){
+        super(nama,imgPath);
+        this.kategori = kategori;
+        this.berat = berat;
+        this.beratPanen = beratPanen;
+    }
+
+    public Hewan(final Hewan other){
+        this(other.getName(),
+            other.getImgPath(),
+            other.getKategori(),
+            other.getBerat(),
+            other.getBeratPanen()
+        );
+    }
     public String getKategori(){
         return this.kategori;
     }
