1e2a4a514fce5a225d98e84f82beb8ecf09c2b6a
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-04 21:20:20 +0700

    feat: deck and petakladang

--- app/src/main/java/deck/Deck.java
+++ app/src/main/java/deck/Deck.java
@@ -4,4 +4,26 @@ import java.util.ArrayList;
 class Deck<T extends Card> {
   private List<T> deck;
 
+  public static final int DECK_SIZE = 40;
+  public static final int DECK_ACTIVE_SIZE = 6;
+
+  public Deck() {
+    deck = new ArrayList<T>(DECK_SIZE);
+  }
+
+  public Deck(int size) {
+    deck = new ArrayList<T>(size);
+  }
+
+  public void addElement(T element) {
+    deck.add(element);
+  }
+
+  public void removeElement(T element) {
+    deck.remove(element);
+  }
+
+  public T getElement(int index) {
+    return deck.get(index);
+  }
 }
--- app/src/main/java/petakladang/PetakLadang.java
+++ app/src/main/java/petakladang/PetakLadang.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 class PetakLadang<T extends Card> {
   private List<List<T>> petakLadang;
-  private static final int MAX_ROW = 4;
-  private static final int MAX_COL = 5;
+  public static final int MAX_ROW = 4;
+  public static final int MAX_COL = 5;
 
   public PetakLadang() {
     petakLadang = new ArrayList<T>(MAX_ROW);
@@ -12,5 +12,13 @@ class PetakLadang<T extends Card> {
     for (int i = 0; i < MAX_ROW; i++) {
       petakLadang.add(new ArrayList<T>(MAX_COL));
     }
+  }
+
+  public void addElement(T element, int row, int col) {
+    petakLadang.get(row).add(col, element);
+  }
+
+  public void removeElement(int row, int col) {
+    petakLadang.get(row).remove(col);
   }
 }
