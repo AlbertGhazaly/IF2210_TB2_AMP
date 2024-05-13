b55144fe9af8a2c1816c4075833184e52a7261f0
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-14 02:16:11 +0700

    feat: field controller

---
+++ src/main/java/com/tubesoop/tubes2oop/FieldController.java
@@ -0,0 +1,16 @@
+package com.tubesoop.tubes2oop;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import deck.Deck;
+import card.Card;
+
+public class FieldController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+
+    }
+}
