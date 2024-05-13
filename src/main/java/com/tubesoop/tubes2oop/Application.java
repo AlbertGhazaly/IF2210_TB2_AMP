638c5c2c5e537da321ce0a696ed450c2a15dac76
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-13 09:57:59 +0700

    feat: turn counter

--- src/main/java/com/tubesoop/tubes2oop/Application.java
+++ src/main/java/com/tubesoop/tubes2oop/Application.java
@@ -1,16 +1,17 @@
 package com.tubesoop.tubes2oop;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
+
+    /* Do Not Change Any Code */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 850, 775);
         stage.setTitle("Tubes 2 OOP");
         stage.setScene(scene);
--- src/main/java/com/tubesoop/tubes2oop/HelloController.java
+++ src/main/java/com/tubesoop/tubes2oop/HelloController.java
@@ -6,9 +6,23 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label turnLabel;
+    private static int numberOfTurn = 1;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    public void updateTurnLabel() {
+        turnLabel.setText(String.valueOf(numberOfTurn));
+    }
+
+    @FXML
+    protected void onNextButtonClick() {
+        numberOfTurn++;
+        updateTurnLabel();
+    }
+
 }
