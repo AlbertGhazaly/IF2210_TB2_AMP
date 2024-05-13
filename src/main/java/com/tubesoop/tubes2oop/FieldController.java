4c8f228d71bb40b1f0f0c8d5c22b62c0c5f77b13
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-14 02:21:43 +0700

    feat: dmmy image on field

--- src/main/java/com/tubesoop/tubes2oop/FieldController.java
+++ src/main/java/com/tubesoop/tubes2oop/FieldController.java
@@ -1,5 +1,6 @@
 package com.tubesoop.tubes2oop;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -7,6 +8,7 @@ import deck.Deck;
 import card.Card;
 
 public class FieldController implements Initializable {
+    @FXML
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
