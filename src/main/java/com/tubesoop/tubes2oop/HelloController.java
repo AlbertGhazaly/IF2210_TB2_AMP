301232769c64277d93817e0e093392aca7642ccb
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-14 01:18:14 +0700

    feat: main

--- src/main/java/com/tubesoop/tubes2oop/HelloController.java
+++ src/main/java/com/tubesoop/tubes2oop/HelloController.java
@@ -1,20 +1,23 @@
 package com.tubesoop.tubes2oop;
 
+import card.Card;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import player.Player;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-    @FXML
     private static int numberOfTurn = 1;
-    private Label turnLabel;
 
+    // FXML Attributes
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Label turnLabel;
+    @FXML
+    private Label player1GuldenLabel;
+    @FXML
+    private Label player2GuldenLabel;
 
+    // FXML Methods
+    @FXML
     public void updateTurnLabel() {
         turnLabel.setText(String.valueOf(numberOfTurn));
     }
@@ -23,6 +26,17 @@ public class HelloController {
     protected void onNextButtonClick() {
         numberOfTurn++;
         updateTurnLabel();
+    }
+
+    public void initialize() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        player1.setGulden(30);
+        player2.setGulden(50);
+
+//        player1GuldenLabel.setText(String.valueOf(player1.getGulden()));
+//        player2GuldenLabel.setText(String.valueOf(player2.getGulden()));
     }
 
 }
---
+++ src/main/java/com/tubesoop/tubes2oop/Main.java
@@ -0,0 +1,7 @@
+package com.tubesoop.tubes2oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Application.class, args);
+    }
+}
