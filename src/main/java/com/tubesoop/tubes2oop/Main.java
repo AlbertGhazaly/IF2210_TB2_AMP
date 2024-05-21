0284d102b75d33b8c837e50aac76961d14894191
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-22 01:58:25 +0700

    fix: player object

--- src/main/java/com/tubesoop/tubes2oop/Main.java
+++ src/main/java/com/tubesoop/tubes2oop/Main.java
@@ -1,7 +1,49 @@
 package com.tubesoop.tubes2oop;
 
-public class Main {
+import gameobject.GameObject;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        Application.launch(Application.class, args);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            GameObject gameObject = new GameObject();
+
+            // Set up the FXMLLoader for the main FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("game.fxml"));
+
+            // Set the controller factory
+            loader.setControllerFactory(controllerClass -> {
+                if (controllerClass == PlayerStatusController.class) {
+                    PlayerStatusController controller = new PlayerStatusController();
+                    controller.setGameObject(gameObject);
+                    return controller;
+                } else {
+                    try {
+                        return controllerClass.getDeclaredConstructor().newInstance();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
+            // Load the main FXML
+            Parent root = loader.load();
+
+            // Set up the stage
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setTitle("Game");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/gameobject/GameObject.java
+++ src/main/java/gameobject/GameObject.java
@@ -7,11 +7,21 @@ public class GameObject {
     private Player player1;
     private Player player2;
     private Toko toko;
+
     public GameObject() {
         this.player1 = new Player();
         this.player2 = new Player();
         this.toko = new Toko();
     }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
     public Toko geToko(){
         return this.toko;
     }
