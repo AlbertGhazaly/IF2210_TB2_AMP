aeadd124c8b9e396447f496a88d269b3958c223c
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-22 03:23:24 +0700

    feat: refresh

--- src/main/java/com/tubesoop/tubes2oop/FieldController.java
+++ src/main/java/com/tubesoop/tubes2oop/FieldController.java
@@ -6,10 +6,43 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
 import player.Player;
 import javafx.scene.Node;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+class DraggableImageView extends ImageView {
+    public DraggableImageView(Image image) {
+        super(image);
+
+        setOnDragDetected(event -> {
+            Dragboard db = startDragAndDrop(TransferMode.ANY);
+            ClipboardContent content = new ClipboardContent();
+            content.putImage(getImage());
+            db.setContent(content);
+            event.consume();
+        });
+
+        setOnDragOver(event -> {
+            if (event.getGestureSource() != this && event.getDragboard().hasImage()) {
+                event.acceptTransferModes(TransferMode.ANY);
+            }
+            event.consume();
+        });
+
+        setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasImage()) {
+                setImage(db.getImage());
+                success = true;
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
+    }
+}
 
 public class FieldController implements Initializable {
     private GameObject gameObject;
@@ -29,7 +62,7 @@ public class FieldController implements Initializable {
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
