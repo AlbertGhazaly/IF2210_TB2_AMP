e90ce8dd594f8099f484f0fb57cce6af10561c51
Albert Ghazaly <albert260306@gmail.com>
2024-05-22 02:36:57 +0700

    feat: all cards load

--- src/main/java/card/Produk.java
+++ src/main/java/card/Produk.java
@@ -3,21 +3,24 @@ package card;
 public class Produk extends Card{
     private int addedBerat;
     private int harga;
+    private String orisinil;
     public Produk() {
         super();
         this.addedBerat = 0;
         this.harga = 0;
     }
-    public Produk(String name, String imgpath, int harga, int addedBerat) {
+    public Produk(String name, String imgpath, int harga, int addedBerat, String orisinil) {
         super(name, imgpath);
         this.addedBerat = addedBerat;
         this.harga = harga;
+        this.orisinil = orisinil;
     }
     public Produk(final Produk other) {
-        this(other.getName(), other.getImgPath(), other.getHarga(), other.getAddedBerat());
+        this(other.getName(), other.getImgPath(), other.getHarga(), other.getAddedBerat(),other.getOrisinil());
     }
     public int getHarga(){return this.harga;}
     public int getAddedBerat(){
         return this.addedBerat;
     }
+    public String getOrisinil(){return this.orisinil;}
 }
--- src/main/java/gameobject/GameObject.java
+++ src/main/java/gameobject/GameObject.java
@@ -26,6 +26,120 @@ public class GameObject {
     public Player getPlayer2() {return this.player2;}
 
     private void initiateCard(){
+        GameObject.hewanList.add(new Hewan("Hiu Darat",
+                "resources/assets/hewan/hiu_darat.png",
+                "karnivora",
+                0,20
+                ));
+        GameObject.hewanList.add(new Hewan("Sapi",
+                "resources/assets/hewan/cow.png",
+                "herbivora",
+                0,10
+        ));
+        GameObject.hewanList.add(new Hewan("Domba",
+                "resources/assets/hewan/sheep.png",
+                "herbivora",
+                0,12
+        ));
+        GameObject.hewanList.add(new Hewan("Kuda",
+                "resources/assets/hewan/horse.png",
+                "herbivora",
+                0,14
+        ));
+        GameObject.hewanList.add(new Hewan("Ayam",
+                "resources/assets/hewan/chicken.png",
+                "omnivora",
+                0,5
+        ));
+        GameObject.hewanList.add(new Hewan("Beruang",
+                "resources/assets/hewan/bear.png",
+                "omnivora",
+                0,25
+        ));
+        GameObject.itemList.add(new Item(
+                "Accelerate",
+                "resources/assets/item/Accelerate.png"
+        ));
+        GameObject.itemList.add(new Item(
+                "Delay",
+                "resources/assets/item/Delay.png"
+        ));
+        GameObject.itemList.add(new Item(
+                "Instant Harvest",
+                "resources/assets/item/Instant_Harvest.png"
+        ));
+        GameObject.itemList.add(new Item(
+                "Destroy",
+                "resources/assets/item/Destroy.png"
+        ));
+        GameObject.itemList.add(new Item(
+                "Protect",
+                "resources/assets/item/Protect.png"
+        ));
+        GameObject.itemList.add(new Item(
+                "Trap",
+                "resources/assets/item/bear_trap.png"
+        ));
+        GameObject.tanamanList.add(new Tanaman(
+                "Biji Jagung",
+                "resources/assets/tanaman/corn_seeds.png",
+                3,0
+        ));
+        GameObject.tanamanList.add(new Tanaman(
+                "Biji Labu",
+                "resources/assets/tanaman/pumpkin_seeds.png",
+                5,0
+        ));
+        GameObject.tanamanList.add(new Tanaman(
+                "Biji Stroberi",
+                "resources/assets/tanaman/strawberry_seeds.png",
+                4,0
 
+        ));
+        GameObject.produkList.add(new Produk(
+                "Sirip Hiu",
+                "resources/assets/produk/shark-fin.png",
+                500,12,"Hiu Darat"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Susu",
+                "resources/assets/produk/susu.png",
+                100,4,"Sapi"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Daging Domba",
+                "resources/assets/produk/Daging_Domba.png",
+                120,6,"Domba"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Daging Kuda",
+                "resources/assets/produk/Daging_Kuda.png",
+                150,8,"Kuda"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Telur",
+                "resources/assets/produk/telur.png",
+                50,2,"Ayam"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Daging Beruang",
+                "resources/assets/produk/Daging_Beruang.png",
+                500,12,"Beruang"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Jagung",
+                "resources/assets/produk/corn.png",
+                150,3,"Biji Jagung"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Labu",
+                "resources/assets/produk/pumpkin.png",
+                500,10,"Biji Labu"
+        ));
+        GameObject.produkList.add(new Produk(
+                "Stroberi",
+                "resources/assets/produk/strawberry.png",
+                350,5,"Biji Stroberi"
+        ));
     }
 }
