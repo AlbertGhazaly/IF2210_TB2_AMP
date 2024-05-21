28f32bce58ca2c97c2ed278f11bdbe01a8eb977a
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-22 04:10:54 +0700

    feat: shuffle

--- src/main/java/com/tubesoop/tubes2oop/ShuffleController.java
+++ src/main/java/com/tubesoop/tubes2oop/ShuffleController.java
@@ -5,24 +5,30 @@ import card.Card;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import player.Player;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ShuffleController implements Initializable {
     GameObject gameObject;
+    Player player1;
+    Player player2;
 
     /* 4 Random Cards */
-    @FXML Card card1;
-    @FXML Card card2;
-    @FXML Card card3;
-    @FXML Card card4;
+    @FXML Pane card1;
+    @FXML Pane card2;
+    @FXML Pane card3;
+    @FXML Pane card4;
 
     /* Shuffle Button */
     @FXML Button shuffleButton;
 
     public void setGameObject(GameObject gameObject) {
         this.gameObject = gameObject;
+        this.player1 = gameObject.getPlayer1();
+        this.player2 = gameObject.getPlayer2();
     }
 
     public void initialize(URL location, ResourceBundle resources) {
