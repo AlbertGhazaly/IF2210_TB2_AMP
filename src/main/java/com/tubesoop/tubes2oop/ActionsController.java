6a587839f02d370f170d9d16975face4e7642261
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-22 19:59:57 +0700

    feat: toko

---
+++ src/main/java/com/tubesoop/tubes2oop/ActionsController.java
@@ -0,0 +1,8 @@
+package com.tubesoop.tubes2oop;
+import com.tubesoop.tubes2oop.TokoController;
+
+public class ActionsController {
+    public void openToko() {
+        TokoController.alandToko.setVisible(true);
+    }
+}
--- src/main/java/com/tubesoop/tubes2oop/TokoController.java
+++ src/main/java/com/tubesoop/tubes2oop/TokoController.java
@@ -1,5 +1,23 @@
 package com.tubesoop.tubes2oop;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TokoController {
+public class TokoController implements Initializable {
+    @FXML public AnchorPane TokoPane;
+    @FXML public Button CloseButton;
 
+    static AnchorPane alandToko;
+
+    public void  closeToko() {
+        TokoPane.setVisible(false);
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+        TokoPane.setVisible(false);
+        alandToko = TokoPane;
+    }
 }
